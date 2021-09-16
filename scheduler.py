cd798d7966e53d9935e5fcb5017086df33f56ac2
lengyue <736917271@qq.com>
2021-09-16 01:07:00 -0400

    optimize

--- scheduler.py
+++ scheduler.py
@@ -23,8 +23,9 @@ if __name__ == "__main__":
 
     # tasks = [Tasks.RecentBattle, Tasks.Battle, Tasks.Home]
     # tasks = [Actions.Home, Actions.Email, Actions.RecentBattle, Actions.Battle, Actions.Home]
-    tasks = [Tasks.Home, Tasks.FriendFoundation]
+    tasks = [Tasks.RecentBattle, Tasks.Battle, Tasks.Home]
     last_frame: Optional[np.ndarray] = None
+    finish_count = 0
 
     while len(tasks) > 0:
         if client.last_frame is None or (client.last_frame == last_frame).all():
@@ -32,13 +33,18 @@ if __name__ == "__main__":
         last_frame = client.last_frame
 
         action, score, (x, y) = predictor.predict(last_frame, tasks[0].value)
+        # visualize_frame(last_frame, (x, y))
         print(action, score)
         if action == Actions.Finish:
             print(f"{tasks[0]} 已完成")
-            tasks.pop(0)
-        elif action == Actions.Touch and score > 0.2:
+            finish_count += 1
+            if finish_count >= 3:
+                tasks.pop(0)
+            time.sleep(1)
+        else:
+            finish_count = 0
+        if action == Actions.Touch and score > 0.1:
             print(tasks[0], action, score, (x, y))
-            # visualize_frame(last_frame, (x, y))
             client.control.touch(x, y, scrcpy.ACTION_DOWN)
             client.control.touch(x, y, scrcpy.ACTION_UP)
             time.sleep(1)
